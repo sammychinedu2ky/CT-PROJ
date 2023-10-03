275f6b04634522f5310a20968f8147bc8f9ba373
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-10-03 16:14:36 +0100

    Add project files.

---
+++ FunnyArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿var manipulateInput = (int[] input) =>
+{
+    var total = 0;
+    int isEvenOrOdd(int number)
+    {
+        return number % 2 == 0 ? total += 1 : total += 3;
+    }
+
+    int isEight(int number)
+    {
+        return number == 8 ? total += 5 : total;
+    }
+    foreach (var number in input)
+    {
+        isEvenOrOdd(number);
+        isEight(number);
+    }
+    return total;
+};
+
+
+var input = new int[] { 1, 2, 3, 4, 5 }; // 11
+var input2 = new int[] { 15, 25, 35 }; // 9
+var input3 = new int[] { 8, 8 }; // 12
+
+foreach (var array in new int[][] { input, input2, input3 })
+{
+    Console.WriteLine(manipulateInput(array));
+}
